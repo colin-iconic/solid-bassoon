fe1de8216f9fbadce3adbf703ab6ed3606ae8075
colin-iconic <43144513+colin-iconic@users.noreply.github.com>
2018-09-19 11:23:54 -0400

    a

--- app.py
+++ app.py
@@ -1490,8 +1490,7 @@ def analytics(name=None):
 	data = pd.DataFrame(data)
 	data = data.set_index(['date'])
 	data = data['price'].resample('W', how='sum')
-	data['date'] = data.index
-	data['date'] = data['date'].astype(str)
+	data['date'] = data.index.to_datetime()
 	data = data.reset_index()
 	data = data.to_dict('records')
 	data = json.dumps(data, indent=2)

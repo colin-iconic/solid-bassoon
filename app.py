822b7f6910cdb8cc1376f417d59e51300dcd27c5
colinmcglone <me@colinmcglone.ca>
2018-10-31 09:28:07 -0400

    a

--- app.py
+++ app.py
@@ -1318,9 +1318,9 @@ def analytics(name=None):
 	connection = pyodbc.connect(r'DRIVER={ODBC Driver 13 for SQL Server};Server=192.168.2.157;DATABASE=Production;UID=support;PWD=lonestar;')
 	cursor = connection.cursor()
 
-	cursor.execute("select order_date, total_price from job where total_price <> 0 and customer not like 'I-H%' and job not like '%-%' and order_date > '1/1/2014 12:00:00 AM' order by Order_Date")
-	query = [list(x) for x in cursor.fetchall()]
-	query = [{'date': x[0], 'price': x[1]} for x in query]
+#	cursor.execute("select order_date, total_price from job where total_price <> 0 and customer not like 'I-H%' and job not like '%-%' and order_date > '1/1/2014 12:00:00 AM' order by Order_Date")
+#	query = [list(x) for x in cursor.fetchall()]
+#	query = [{'date': x[0], 'price': x[1]} for x in query]
 
 #	data = pd.DataFrame(query)
 #	data = data.set_index(['date'])
@@ -1330,12 +1330,12 @@ def analytics(name=None):
 #	data = data.fillna(0)
 #	data = data.to_dict('records')
 #	data = data[0:-1]
-	data = query
-	for e in data:
-		e['date'] = e['date'].strftime('%Y-%m-%d')
+#	data = query
+#	for e in data:
+#		e['date'] = e['date'].strftime('%Y-%m-%d')
 
-	data = json.dumps(data, indent=2, default=str)
-	data = {'data': data}
+#	data = json.dumps(data, indent=2, default=str)
+#	data = {'data': data}
 
 	#cursor.execute("SELECT CHANGE_HISTORY.CHANGED_BY, CHANGE_HISTORY.CHANGE_DATE, CHANGE_HISTORY.OLD_TEXT, CHANGE_HISTORY.NEW_TEXT, CHANGE_HISTORY.WC_VENDOR, CHANGE_HISTORY.JOB, JOB.CUSTOMER, JOB.DESCRIPTION, JOB.PART_NUMBER, JOB.ORDER_QUANTITY FROM (CHANGE_HISTORY INNER JOIN JOB ON CHANGE_HISTORY.JOB = JOB.JOB) WHERE CHANGE_HISTORY.CHANGE_DATE > DATEADD(DAY, DATEDIFF(DAY, 0, getDate() - 30), 0) AND CHANGE_HISTORY.CHANGE_TYPE = 14")
 

0cca09134ae1310c926c8890e6f013ffcb155bb8
colinmcglone <me@colinmcglone.ca>
2018-12-04 13:26:06 -0500

    a

--- app.py
+++ app.py
@@ -1727,11 +1727,11 @@ def mobile_traveler(name=None):
 	cursor = connection.cursor()
 
 	cursor.execute("select status, part_number, order_quantity, customer_po, customer, ship_to, note_text, cast(order_date as date) from job where job = '{0}'".format(job))
-	data = [list(x) for x in cursor.fetchall()][0]
-	#job_details = {'job': job, 'status': 1, 'part number': 2, 'quantity': 3, 'customer po': 4, 'customer': 5, 'ship to': 6, 'note text': 7}
-
-	if data == []:
+	data = list(cursor.fetchall())
+	if not data:
 		return render_template('mobile_traveler.html', job_details = {'job': 'Invalid Job - Try Again'})
+
+	data = [list(x) for x in cursor.fetchall()][0]
 
 	job_details = {'job': job, 'status': data[0], 'part number': data[1], 'quantity': data[2], 'customer po': data[3], 'customer': data[4], 'ship to': data[5], 'note text': data[6], 'order date': data[7]}
 

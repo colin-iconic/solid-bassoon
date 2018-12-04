5321023935c8026f6060071f04bb3e24908ed717
colinmcglone <me@colinmcglone.ca>
2018-12-04 12:54:59 -0500

    a

--- app.py
+++ app.py
@@ -1726,11 +1726,11 @@ def mobile_traveler(name=None):
 	connection = pyodbc.connect(r'DRIVER={ODBC Driver 13 for SQL Server};Server=192.168.2.157;DATABASE=Production;UID=support;PWD=lonestar;')
 	cursor = connection.cursor()
 
-	cursor.execute("select status, part_number, order_quantity, customer_po, customer, ship_to, note_text from job where job = '{0}'".format(job))
+	cursor.execute("select status, part_number, order_quantity, customer_po, customer, ship_to, note_text, cast(order_date as date) from job where job = '{0}'".format(job))
 	data = [list(x) for x in cursor.fetchall()][0]
 	#job_details = {'job': job, 'status': 1, 'part number': 2, 'quantity': 3, 'customer po': 4, 'customer': 5, 'ship to': 6, 'note text': 7}
 
-	job_details = {'job': job, 'status': data[0], 'part number': data[1], 'quantity': data[2], 'customer po': data[3], 'customer': data[4], 'ship to': data[5], 'note text': data[6]}
+	job_details = {'job': job, 'status': data[0], 'part number': data[1], 'quantity': data[2], 'customer po': data[3], 'customer': data[4], 'ship to': data[5], 'note text': data[6], 'order date': data[7]}
 
 	cursor.execute("select name, line1, line2, city, state, zip from address where address = '{0}'".format(job_details['ship to']))
 	job_details['address'] = [x for x in cursor.fetchall()][0]

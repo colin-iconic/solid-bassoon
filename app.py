763d31a8ea824c1e09cc831e25a025e8ff474387
colinmcglone <me@colinmcglone.ca>
2019-01-10 16:05:32 -0500

    Fix header

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def hotlist(name=None):
 		centers.sort(key=itemgetter(8))
 		rows.append(centers[0])
 
-	head = ['Priority', 'Job Number', 'Customer', 'Customer PO' 'Description', 'Order Date', 'Order Quantity', 'Part Number', 'Work Center', 'Sequence']
+	head = ['Priority', 'Job Number', 'Customer', 'Customer PO', 'Description', 'Order Date', 'Order Quantity', 'Part Number', 'Work Center', 'Sequence']
 	return render_template('hot.html', rows = rows, head = head, title = 'Hot List')
 
 @app.route('/racks')

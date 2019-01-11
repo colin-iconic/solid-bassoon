4ffe19a33560461ee33087e43e891b126b23f8e1
colinmcglone <me@colinmcglone.ca>
2019-01-11 07:00:34 -0500

    a

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def hotlist(name=None):
 
 	for job in data:
 		try:
-			cursor.execute("select work_center, sequence from job_operation where job = '{0}' and job_operation.status = 'o'".format(job[0]))
+			cursor.execute("select work_center, sequence from job_operation where job = '{0}' and job_operation.status = 'o'".format(job[1]))
 			job_data = [list(x) for x in cursor.fetchall()]
 			job_data.sort(key=itemgetter(1))
 			job.append(job_data[0][0])

87fa1ec55e5f74b21b7ad66858fa228578edbfe4
colinmcglone <me@colinmcglone.ca>
2019-01-17 09:16:50 -0500

    a

--- app.py
+++ app.py
@@ -2011,7 +2011,7 @@ def quotes(name=None):
 	quote_date = []
 	for quote in data:
 		cursor.execute("select total_price from quote_qty where quote like '%{0}%'".format(quote[0]))
-		quote_data = [list(x) for x in cursor.fetchall()]
+		quote_data = [list(x)[0] for x in cursor.fetchall()]
 		quote.extend(quote_data)
 
 		if quote[3] == 2: #if currency is CAD do nothing

24bbc8a340894c6b870168c61a6d66a171f8ac8f
colinmcglone <me@colinmcglone.ca>
2019-01-22 16:53:09 -0500

    a

--- app.py
+++ app.py
@@ -2080,7 +2080,7 @@ def testing(name=None):
 		elif quote[9] == 1: #if currency is USD convert to CAD
 			quote[11] = Decimal(quote[11])*Decimal(1.3)
 
-	quotes = {'quotes_per_week': 0, 'total_value': 0, 'total_win': 0, 'customers': [], 'customer_counts': {}, 'customer_total': {}, 'customer_wins': {}, 'quotes': {}}
+	quotes = {'quotes_per_week': 0, 'total_value': 0, 'total_win': 0, 'customers': [], 'customer_counts': {}, 'customer_total': {}, 'customer_wins': {}, 'quotes': []]}
 
 	for quote in data:
 		quotes['quotes_per_week'] += 1
@@ -2103,7 +2103,7 @@ def testing(name=None):
 			if quote[3] == 'Won':
 				quotes['customer_wins'][quote[5]] += 1
 
-		quotes['quotes'][quote[0]] = {'part_number': quote[2], 'quantity': quote[10], 'total_price': quote[11], 'quote': quote[0], 'status': quote[3], 'quoted_by': quote[1], 'date': quote[7], 'reference': quote[8]}
+		quotes['quotes'].append({'part_number': quote[2], 'quantity': quote[10], 'total_price': quote[11], 'quote': quote[0], 'status': quote[3], 'quoted_by': quote[1], 'date': quote[7], 'reference': quote[8]})
 
 	cursor.execute("select quote.quote, quote.rfq, cast(rfq.quote_date as date), rfq.trade_currency, quote.status from quote inner join rfq on quote.rfq = rfq.rfq where rfq.quote_date > DATEADD(DAY, DATEDIFF(DAY, 0, getDate() - 365), 0) and rfq.customer = '{0}'".format(cust))
 	data = [list(x) for x in cursor.fetchall()]

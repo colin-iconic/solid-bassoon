0b56312dd9b3d1c89c0856abae4b6d02946ea050
colinmcglone <me@colinmcglone.ca>
2019-02-12 14:02:01 -0500

    a

--- app.py
+++ app.py
@@ -2208,7 +2208,7 @@ def update_mailer():
 
 	cursor = connection.cursor()
 
-	cursor.execute("select job.job, user_values.text3, user_values.text4 from user_values left join job on user_values.user_values = job.user_values left join change_history on job.job = change_history.job where job.user_values not like 'None' and user_values.text3 not like 'None' and user_values.text4 not like 'None' and change_history.change_type = '14' and change_history.change_date > DATEADD(HOUR, -1, GETDATE())")
+	cursor.execute("select job.job, user_values.text3, user_values.text4 from user_values left join job on user_values.user_values = job.user_values left join change_history on job.job = change_history.job where job.user_values not like 'None' and user_values.text3 not like 'None' and user_values.text4 not like 'None' and change_history.change_type = '14' and change_history.change_date > DATEADD(HOUR, -1, GETDATE()) and change_history.new_text = 'C'")
 	data = [list(x) for x in cursor.fetchall()]
 
 	update_jobs = []

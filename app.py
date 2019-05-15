236e68bef8c7e2add961f4bd1b1fb5a744ccc725
colinmcglone <me@colinmcglone.ca>
2019-05-15 10:19:42 -0400

    a

--- app.py
+++ app.py
@@ -2499,7 +2499,7 @@ def orders_report():
 
     cursor.execute("select cast(packlist_header.packlist_date as date), packlist_detail.unit_price, packlist_detail.quantity, job.trade_currency from (packlist_header inner join packlist_detail on packlist_header.packlist = packlist_detail.packlist) left join job on packlist_detail.job = job.job where Job.Customer Not Like '%GARAGESCAP%' And Job.Customer Not Like '%I-H%' AND packlist_header.packlist_date > Dateadd(month, -4, getdate()) AND Job.Job Not Like '%-%' order by packlist_header.packlist_date desc")
 
-    data = [list(x) for x in cursor.fetchall()
+    data = [list(x) for x in cursor.fetchall()]
 
     for each in data:
         print(each)

e54e42c5f6d424f4464e2eb5085cdc1bd75bc5a7
colinmcglone <me@colinmcglone.ca>
2019-05-16 09:22:47 -0400

    a

--- app.py
+++ app.py
@@ -2569,25 +2569,25 @@ def orders_report():
     data = [list(x) for x in cursor.fetchall()]
     for each in data:
         try:
-            if each[3] == 1: #if currency is USD convert to CAD
+            if each[2] == 1: #if currency is USD convert to CAD
                 each[1] = Decimal(each[1])*Decimal(1.27)
             else:
                 pass
         except:
             each[1] = 0
-            
+
         price = round(each[1] * each[2], 2)
         top_shipments.append({'customer': each[3], 'part': each[4], 'description': each[5], 'price': price, 'job': each[6]})
 
-    top_shipments = top_shipments.sort(key=itemgetter('price'))[:4]
-    chart_data['top_shipments'] = top_shipments
+    top_shipments = top_shipments.sort(key=itemgetter('price'))
+    chart_data['top_shipments'] = top_shipments[:4]
 
     cursor.execute("select job.total_price, job.quantity, job.trade_currency, job.customer, job.part_number, job.description, job.job from (packlist_header inner join packlist_detail on packlist_header.packlist = packlist_detail.packlist) left join job on packlist_detail.job = job.job where Job.Customer Not Like '%GARAGESCAP%' And Job.Customer Not Like '%I-H%' AND job.order_date > Dateadd(week, -1, getdate()) AND Job.Job Not Like '%-%'")
     top_orders = []
     data = [list(x) for x in cursor.fetchall()]
     for each in data:
         try:
-            if each[3] == 1: #if currency is USD convert to CAD
+            if each[2] == 1: #if currency is USD convert to CAD
                 each[1] = Decimal(each[1])*Decimal(1.27)
             else:
                 pass
@@ -2596,8 +2596,8 @@ def orders_report():
         price = round(each[1] * each[2], 2)
         top_orders.append({'customer': each[3], 'part': each[4], 'description': each[5], 'price': price, 'job': each[6]})
 
-    top_orders = top_orders.sort(key=itemgetter('price'))[:4]
-    chart_data['top_orders'] = top_orders
+    top_orders = top_orders.sort(key=itemgetter('price'))
+    chart_data['top_orders'] = top_orders[:4]
 
     return render_template('orders_report.html', data = chart_data)
 

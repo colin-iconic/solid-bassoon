ae99b7e4e09ecad4e438898b6924f2c161ddd19f
colinmcglone <me@colinmcglone.ca>
2019-05-23 13:54:37 -0400

    a

--- app.py
+++ app.py
@@ -2421,7 +2421,7 @@ def update_mailer():
                 else:
                     job.append(data[0][0])
 
-                job.append([data[0][1],data[-1][1]])
+                job.append((data[0][1]/data[-1][1])*100)
 
             update_jobs.append(job)
 

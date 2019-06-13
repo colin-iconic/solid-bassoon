a7c4837b141b6c9a5e03a2270d8a3967f1b192d1
colinmcglone <me@colinmcglone.ca>
2019-06-13 09:26:06 -0400

    a

--- app.py
+++ app.py
@@ -2747,14 +2747,14 @@ def production_review(name=None):
     for wc in wc_data:
         if wc[4] == 'o':
             if wc[0] not in jobs:
-                jobs[wc[0]] = {'current': {'work_center': wc[2], 'sequence': wc[3], 'updated': wc[5]}, 'previous': {}}
+                jobs[wc[0]]['current'] = {'work_center': wc[2], 'sequence': wc[3], 'updated': wc[5]}
         if wc[4] == 'c':
             if wc[0] not in jobs:
-                jobs[wc[0]] = {'current': {}, 'previous': {'work_center': wc[2], 'sequence': wc[3], 'updated': wc[5]}}
+                jobs[wc[0]]['previous'] = {'work_center': wc[2], 'sequence': wc[3], 'updated': wc[5]}}
         elif wc[4] == 'o' and jobs[wc[0]]['current']['sequence'] > wc[3]:
-            jobs[wc[0]] = {'current': {'work_center': wc[2], 'sequence': wc[3], 'updated': wc[5]}}
-        elif wc[4] == 'c' and jobs[wc[0]]['current']['sequence'] < wc[3]:
-            jobs[wc[0]] = {'current': {'work_center': wc[2], 'sequence': wc[3], 'updated': wc[5]}}
+            jobs[wc[0]]['current'] =  {'work_center': wc[2], 'sequence': wc[3], 'updated': wc[5]}}
+        elif wc[4] == 'c' and jobs[wc[0]]['previous']['sequence'] < wc[3]:
+            jobs[wc[0]]['previous'] =  {'work_center': wc[2], 'sequence': wc[3], 'updated': wc[5]}}
 
     for job in jobs:
         if job['current']['work_center'] == 'SCHEDULE':
